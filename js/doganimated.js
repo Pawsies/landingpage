b236fe776d6f67b3594d2d56fec16ba3f4e513be
David <rodavidfernandez@gmail.com>
2017-07-07 22:25:18 -0300

    add web

---
+++ js/doganimated.js
@@ -0,0 +1,8 @@
+// Dribbble shot by Richard Perez: http://dribbble.com/shots/1343438-Gus-Gif
+
+// Code by Helen V. Holmes
+// helenvholmes.co
+
+// Help from these lovely folks:
+// http://stackoverflow.com/users/935361/brian-phillips
+// http://stackoverflow.com/users/2642616/josh-powell
